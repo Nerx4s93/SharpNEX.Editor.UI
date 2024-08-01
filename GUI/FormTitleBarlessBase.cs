0b8a29da4c62b23ce9de093aa65c7e3a3c5f4e61
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2024-08-01 14:00:28 +0300

    Форма с уменьшеным заголовком

---
+++ GUI/FormTitleBarlessBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace SharpNEX.Editor.UI.GUI
+{
+    public partial class FormTitleBarlessBase : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        const int GWL_STYLE = -16;
+        const int WS_CAPTION = 0x00C00000;
+
+        public FormTitleBarlessBase()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IntPtr hwnd = this.Handle;
+            int style = GetWindowLong(hwnd, GWL_STYLE);
+            SetWindowLong(hwnd, GWL_STYLE, style & ~WS_CAPTION);
+
+            this.Width += 1;
+            this.Width -= 1;
+        }
+    }
+}
