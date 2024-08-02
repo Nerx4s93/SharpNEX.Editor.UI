f90ab1cf767860afb9016e549660deeb85776c53
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2024-08-02 11:13:11 +0300

    Создание экземпляра с помощью коардинат

--- Data/Line.cs
+++ Data/Line.cs
@@ -4,11 +4,22 @@ namespace SharpNEX.Editor.UI.Data
 {
     public class Line
     {
+        public Line()
+        {
+
+        }
+
+        public Line(int x1, int y1, int x2, int y2)
+        {
+            PointStart = new Point(x1, y1);
+            PointEnd = new Point(x2, y2);
+        }
+
         public Point PointStart { get; set; } = Point.Empty;
 
         public Point PointEnd { get; set; } = Point.Empty;
 
-        public Color Color { get; set; } = Color.White;
+        public Color Color { get; set; } = Color.FromArgb(203, 203, 203);
 
         public float Width { get; set; } = 1f;
 
