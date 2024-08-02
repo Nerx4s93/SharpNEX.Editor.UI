07491f2add0735296e7c97968ffd27e2b1a8e63b
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2024-08-02 09:56:10 +0300

    Класс для отрисовки линии

---
+++ Data/Line.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace SharpNEX.Editor.UI.Data
+{
+    internal class Line
+    {
+        public Point PointStart { get; set; } = Point.Empty;
+
+        public Point PointEnd { get; set; } = Point.Empty;
+
+        public Color Color { get; set; } = Color.White;
+
+        public float Width { get; set; } = 1f;
+
+        public void DrawLine(Graphics graphics)
+        {
+            using (var pen = new Pen(Color, Width))
+            {
+                graphics.DrawLine(pen, PointStart, PointEnd);
+            }
+        }
+    }
+}
