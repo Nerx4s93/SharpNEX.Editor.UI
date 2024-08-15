d67e44dfa280a78766bde31d3a4f5660afe84ccc
Никита Хуснутдинов <nikitahysnutdinov@gmail.com>
2024-08-15 07:22:33 +0300

    Изменение названия класса

---
+++ MenuStripTheme.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace SharpNEX.Editor.UI
+{
+    public class MenuStripTheme : MenuStrip
+    {
+        private ToolStripRenderer _darkStyleRenderer = new ToolStripRenderer(Color.White, Color.FromArgb(31, 31, 31), Color.FromArgb(61, 61, 61), Color.FromArgb(46, 46, 46));
+
+        public MenuStripTheme()
+        {
+            Renderer = _darkStyleRenderer;
+        }
+    }
+}
