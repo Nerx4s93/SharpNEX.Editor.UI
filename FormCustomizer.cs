af2cd98d3f9416f24c45a96a4e92030db2bfa360
Nerx <nikitahysnutdinov@gmail.com>
2024-11-25 11:56:23 +0300

    Класс для костамизации формы

---
+++ FormCustomizer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace SharpNEX.Editor.UI
+{
+    public static class FormCustomizer
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+
+        public static void RoundEdges(Form form)
+        {
+            form.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, form.Width, form.Height, 20, 20));
+        }
+    }
+}
