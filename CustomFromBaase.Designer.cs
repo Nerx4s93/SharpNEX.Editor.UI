7e2edc8da898127365358c75f46c0c6e0c1834ff
Nerx <nikitahysnutdinov@gmail.com>
2024-11-27 16:34:54 +0300

    Базово настроенная форма

---
+++ CustomFromBaase.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace SharpNEX.Editor.UI
+{
+    partial class CustomFromBaase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CustomFromBaase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CustomFromBaase";
+            this.Text = "CustomFromBaase";
+            this.Shown += new System.EventHandler(this.CustomFromBaase_Shown);
+            this.Resize += new System.EventHandler(this.CustomFromBaase_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ CustomFromBaase.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+
+namespace SharpNEX.Editor.UI
+{
+    public partial class CustomFromBaase : Form
+    {
+        public CustomFromBaase()
+        {
+            InitializeComponent();
+        }
+
+        private void CustomFromBaase_Shown(object sender, System.EventArgs e)
+        {
+            FormCustomizer.RoundEdges(this);
+        }
+
+        private void CustomFromBaase_Resize(object sender, System.EventArgs e)
+        {
+            FormCustomizer.RoundEdges(this);
+        }
+    }
+}
